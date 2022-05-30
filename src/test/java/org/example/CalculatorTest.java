b624cd05b6f5136a27f5a8ac916fb46b07811d5a
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-30 15:49:53 +0700

    before after each

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,8 +1,7 @@
 package org.example;
 
 import org.example.generator.SimpleDisplayNameGenerator;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,6 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 class CalculatorTest {
     private final Calculator calculator = new Calculator();
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("After each");
+    }
 
     @Test
 //    @DisplayName("Test skenario sukses untuk method add(integer, integer)")
@@ -26,5 +35,11 @@ class CalculatorTest {
     @Test
     void testDivideByZero() {
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
+    }
+
+    @Test
+    @Disabled
+    public void testComingSoon() {
+        assertEquals(0, calculator.divide(4, 2));
     }
 }
