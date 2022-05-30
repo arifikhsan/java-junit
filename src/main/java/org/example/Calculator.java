dc07019d0f94b6e06478f7b00a5b67bedd01e5a0
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-30 15:22:07 +0700

    display name

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,4 +4,11 @@ public class Calculator {
     public Integer add(Integer first, Integer second) {
         return first + second;
     }
+
+    public Integer divide(Integer first, Integer second) {
+        if (second == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return first / second;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,14 +1,30 @@
 package org.example;
 
+import org.example.generator.SimpleDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+//@DisplayName("Tes Calculator")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 class CalculatorTest {
+    private final Calculator calculator = new Calculator();
 
     @Test
+//    @DisplayName("Test skenario sukses untuk method add(integer, integer)")
     void add() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.add(1, 2));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2, calculator.divide(4, 2));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
     }
 }
