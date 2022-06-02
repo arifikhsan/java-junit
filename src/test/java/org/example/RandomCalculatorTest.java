12c74930e041f92ea560d5576c6470edf401abd7
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-06-02 13:29:23 +0700

    test parameter

--- src/test/java/org/example/RandomCalculatorTest.java
+++ src/test/java/org/example/RandomCalculatorTest.java
@@ -2,10 +2,15 @@ package org.example;
 
 import org.example.Calculator;
 import org.example.resolver.RandomParameterResolver;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,5 +30,27 @@ public class RandomCalculatorTest {
         System.out.println(a + b);
         System.out.println(testInfo.getDisplayName());
         assertEquals(a + b, calculator.add(a, b));
+    }
+
+    @DisplayName("Tes random calculator")
+    @ParameterizedTest(name = "{displayName} ke {index} dari {arguments} dengan parameter {0}")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void testWithParameter(int value) {
+        var expected = value + value;
+        var actual = calculator.add(value, value);
+        assertEquals(expected, actual);
+    }
+
+    public static List<Integer> parameterSource() {
+        return List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    @DisplayName("Tes random calculator")
+    @ParameterizedTest(name = "{displayName} ke {index} dari {arguments} dengan parameter {0}")
+    @MethodSource("parameterSource")
+    void testWithMethodSource(int value) {
+        var expected = value + value;
+        var actual = calculator.add(value, value);
+        assertEquals(expected, actual);
     }
 }
